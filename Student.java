10a771b13e4786a962aef46e86899ac210a25952
Shubhangi Bhor <shubhanginb24301@gmail.com>
2025-04-03 14:14:45 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,35 @@
+package sms;
+import java.io.*;
+//import java.util.*;
+class Student implements Serializable {
+    private String name;
+    private int rollNumber;
+    private String grade;
+
+    public Student(String name, int rollNumber, String grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Roll No: " + rollNumber + ", Name: " + name + ", Grade: " + grade;
+    }
+}
